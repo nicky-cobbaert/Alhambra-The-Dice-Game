ae2e8077891ab40bc64c211f02ca548d70207b22
WOUT GHEYSELS <WOUT.GHEYSELS@student.hogent.be>
2025-02-20 21:03:16 +0100

    Databank koppelen aan eclipse
    Ik heb de gegevens aangepast

--- src/persistentie/Connectie.java
+++ src/persistentie/Connectie.java
@@ -4,10 +4,12 @@ public class Connectie {
 
     /*
      * INFO: Hieronder dien je uiteraard het schema, de username en het paswoord nog aan te passen.
-     * => vichogent.be:40094/G100
-     * => G100
-     * => rNwn1z4CpVBSBYRzkPZwz7SkBcf6efrW
+     * => vichogent.be:40094/G74
+     * => G74
+     * => RyYNGVMqAI0m06amurTA0MQwfm3yG8XZ
      */
  
-    public static final String JDBC_URL = "jdbc:mysql://vichogent.be:40094/G100?user=G100&password=rNwn1z4CpVBSBYRzkPZwz7SkBcf6efrW";
+   // public static final String JDBC_URL = "jdbc:mysql://vichogent.be:40094/G100?user=G100&password=rNwn1z4CpVBSBYRzkPZwz7SkBcf6efrW"; --> orginele ik heb het aangepast naar ons ding
+    
+    public static final String JDBC_URL = "jdbc:mysql://vichogent.be:40094/G74?user=G74&password=RyYNGVMqAI0m06amurTA0MQwfm3yG8XZ";
 }
