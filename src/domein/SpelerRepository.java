df1f21236ae8c2e464e0fca61247eb2c0053a921
Sverre Lippens <Sverre.Lippens@student.hogent.be>
2025-03-01 12:34:05 +0100

    MySQL gekoppeld aan de code
    Is getest en werkt, kan je zien door select * from speler te doen in MySQL workbench.

--- src/domein/SpelerRepository.java
+++ src/domein/SpelerRepository.java
@@ -10,14 +10,14 @@ public class SpelerRepository {
     private final SpelerMapper mapper;
     
     //Deze lijst is voor het nakijken of de gebruikersnaam al bestaat. Als de database er is kan dit allemaal weg
-    private List<Speler> spelers;
+//    private List<Speler> spelers;
 
     public SpelerRepository() 
     {
         mapper = new SpelerMapper();
         
         //Dit is voor de 'DTO'
-        spelers=mapper.geefAlleSpelers();
+//        spelers=mapper.geefAlleSpelers();
     }
     
     public void voegToe(Speler speler) {
@@ -25,22 +25,22 @@ public class SpelerRepository {
     	    throw new GebruikersnaamInGebruikException();
        }
                  
-//       mapper.voegToe(speler); 	//Dit wordt pas gebruikt als de database ingesteld is. Nu wordt een DTO gebruikt
+       mapper.voegToe(speler); 	//Dit wordt pas gebruikt als de database ingesteld is. Nu wordt een DTO gebruikt
        
        // Dit is voor de 'DTO'
-       spelers.add(speler);
+//       spelers.add(speler);
     }
 
     private boolean bestaatSpeler(String gebruikersnaam){
-//        return mapper.geefSpeler(gebruikersnaam)!=null;
+        return mapper.geefSpeler(gebruikersnaam)!=null;
     	
     	//Dit is voor de 'DTO' 	
-    	for (Speler s : spelers) {
-     	   if (gebruikersnaam.contains(s.getGebruikersnaam())) {
-     		   return true;
-     	   }
-        }
-    	return false;
+//    	for (Speler s : spelers) {
+//     	   if (gebruikersnaam.contains(s.getGebruikersnaam())) {
+//     		   return true;
+//     	   }
+//        }
+//    	return false;
     }  
 }
 
--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -12,59 +12,59 @@ import domein.Speler;
 
 public class SpelerMapper {
 
-    private static final String INSERT_SPELER = "INSERT INTO G74.speler (gebruikersnaam, geboortejaar, aantalGewonnen, aantalGespeeld)"
-            + "VALUES (?, ?, ?, ?)";
+    private static final String INSERT_SPELER = "INSERT INTO speler (Gebruikers, Geboortejaar, AantalGewonnen, AantalGespeeld) VALUES (?, ?, ?, ?)";
     
-    private static final String GEEF_SPELER = "SELECT * FROM G74.speler WHERE gebruikersnaam = ?";
+    private static final String GEEF_SPELER = "SELECT * FROM speler WHERE Gebruikers = ?";
+    
             
-//    public void voegToe(Speler speler) 
-//    {
-//    	try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL);
-//                PreparedStatement query = conn.prepareStatement(INSERT_SPELER)) 
-//        {
-//            query.setString(1, speler.getGebruikersnaam());
-//            query.setInt(2, speler.getGeboortejaar());
-//            query.setInt(3, speler.getAantalGewonnen());
-//            query.setInt(4, speler.getAantalGespeeld());
-//            
-//            query.executeUpdate();
-//
-//        } catch (SQLException ex) {
-//            throw new RuntimeException(ex);
-//        }
-//    }
-//
-//    
-//    public Speler geefSpeler(String gebruikersnaam) {
-//        Speler speler = null;
-//
-//        try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL);
-//                PreparedStatement query = conn.prepareStatement(GEEF_SPELER)) {
-//            query.setString(1, gebruikersnaam);
-//            try (ResultSet rs = query.executeQuery()) {
-//                if (rs.next()) 
-//                {
-//                    int geboortejaar = rs.getInt("geboortejaar");
-//                    int aantalGewonnen = rs.getInt("aantalGewonnen");
-//                    int aantalGespeeld = rs.getInt("aantalGespeeld");
-//
-//                    speler = new Speler(gebruikersnaam, geboortejaar, aantalGewonnen, aantalGespeeld);               
-//                }
-//            }
-//        } catch (SQLException ex) {
-//            throw new RuntimeException(ex);
-//        }
-//
-//        return speler;
-//    }
+    public void voegToe(Speler speler) 
+    {
+    	try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL);
+                PreparedStatement query = conn.prepareStatement(INSERT_SPELER)) 
+        {
+            query.setString(1, speler.getGebruikersnaam());
+            query.setInt(2, speler.getGeboortejaar());
+            query.setInt(3, speler.getAantalGewonnen());
+            query.setInt(4, speler.getAantalGespeeld());
+            
+            query.executeUpdate();
+
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    
+    public Speler geefSpeler(String gebruikersnaam) {
+        Speler speler = null;
+
+        try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL);
+                PreparedStatement query = conn.prepareStatement(GEEF_SPELER)) {
+            query.setString(1, gebruikersnaam);
+            try (ResultSet rs = query.executeQuery()) {
+                if (rs.next()) 
+                {
+                    int geboortejaar = rs.getInt("geboortejaar");
+                    int aantalGewonnen = rs.getInt("aantalGewonnen");
+                    int aantalGespeeld = rs.getInt("aantalGespeeld");
+
+                    speler = new Speler(gebruikersnaam, geboortejaar, aantalGewonnen, aantalGespeeld);               
+                }
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return speler;
+    }
 
     //Dit is voor de DTO te kunnen opstellen. Als de databank werkt mag dit weg!
-    public List<Speler> geefAlleSpelers(){
-    	List<Speler> spelers = new ArrayList<>();
-    	
-    	spelers.add(new Speler("bestaatAl",2003));
-    	spelers.add(new Speler("bestaatOokAl",1999,4,10));
-    	
-    	return spelers;
-    }
+//    public List<Speler> geefAlleSpelers(){
+//    	List<Speler> spelers = new ArrayList<>();
+//    	
+//    	spelers.add(new Speler("bestaatAl",2003));
+//    	spelers.add(new Speler("bestaatOokAl",1999,4,10));
+//    	
+//    	return spelers;
+//    }
 }
