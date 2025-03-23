4d3a8a10b48a4999675702232c62085c3d7160eb
Nicky Cobbaert <Nicky.Cobbaert@student.hogent.be>
2025-03-23 17:28:05 +0100

    zetsteen: plaatsNeer wijzigt nu de positie

--- src/domein/Zetsteen.java
+++ src/domein/Zetsteen.java
@@ -8,6 +8,11 @@ public class Zetsteen {
 		
 	}
 	
+	public void plaatsNeer(int positie) {
+		
+		this.positie = positie;
+	}
+	
 	
 	public int getPositie() { // Snel getter toegevoegd voor mijn methode
         return positie;
