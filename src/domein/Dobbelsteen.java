ff6baa61f93c24d2750e864ef7983fabc36aefbb
Nicky Cobbaert <Nicky.Cobbaert@student.hogent.be>
2025-03-23 17:00:12 +0100

    Dobbelsteen geimplementeerd

--- src/domein/Dobbelsteen.java
+++ src/domein/Dobbelsteen.java
@@ -1,12 +1,24 @@
 package domein;
 
+import java.security.SecureRandom;
+
+import utils.DobbelsteenKleur;
+
 public class Dobbelsteen {
 
-	private String kleur;
-	private int waarde;
-	
+	private DobbelsteenKleur kleur;
+
 	public Dobbelsteen() {
-		
+
 	}
-	
+
+	public DobbelsteenKleur getDobbelsteenKleur() {
+		return this.kleur;
+	}
+
+	public void dobbel() {
+		int index = new SecureRandom().nextInt(1, DobbelsteenKleur.values().length);
+		this.kleur = DobbelsteenKleur.values()[index];
+	}
+
 }
---
+++ src/testen/DobbelsteenTest.java
@@ -0,0 +1,9 @@
+package testen;
+
+import org.junit.jupiter.api.Test;
+
+public class DobbelsteenTest {
+
+//TODO
+	
+}
