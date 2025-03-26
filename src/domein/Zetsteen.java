0d574bf50b239004f32e5358de28ce382e02d9bd
Nicky Cobbaert <Nicky.Cobbaert@student.hogent.be>
2025-03-26 22:08:50 +0100

    De Zetsteen controleert nu op foutieve posities
    Zetsteen test heeft dit geconfirmeerd.

--- src/domein/Zetsteen.java
+++ src/domein/Zetsteen.java
@@ -1,21 +1,44 @@
 package domein;
 
 public class Zetsteen {
-	
-	private int positie; 
-	
+
+	private int positie;
+
 	public Zetsteen() {
-		
+
 	}
+
+	/**
+	 * Kleur moet tussen 1-6 liggen. 
+	 * Zetsteen positie Y moet tussen 1-8 liggen.
+	 * Zetsteen positie X moet tussen 1-3 liggen.
+	 * 
+	 */
+
 	
-	public void plaatsNeer(int positie) {
-		
+	public void plaatsNeer(int positie) throws IllegalArgumentException {
+
+		if (positie < 111 || positie > 683) {
+			throw new IllegalArgumentException(
+					"De positie moet een kleur tussen [1,6] , verticale positie tussen [1,8] en een horizontale positie tussen [1,3] hebben!");
+		}
+		if (((positie / 100) % 10) < 1 || ((positie / 100) % 10) > 6) {
+			throw new IllegalArgumentException("De kleur moet tussen 1 en 6 liggen!");
+		}
+
+		if (((positie / 10) % 10) < 1 || ((positie / 10) % 10) > 8) {
+			throw new IllegalArgumentException("De verticale positie moet tussen 1 en 8 liggen!");
+		}
+
+		if ((positie % 10) < 1 || (positie % 10) > 3) {
+			throw new IllegalArgumentException("De horizontale positie moet tussen 1 en 3 liggen!");
+		}
+
 		this.positie = positie;
 	}
-	
-	
+
 	public int getPositie() { // Snel getter toegevoegd voor mijn methode
-        return positie;
-    }
+		return positie;
+	}
 
 }
--- src/testen/ZetsteenTest.java
+++ src/testen/ZetsteenTest.java
@@ -1,6 +1,12 @@
 package testen;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import domein.Zetsteen;
 
 public class ZetsteenTest {
 
@@ -9,11 +15,109 @@ public class ZetsteenTest {
 	 * krijgen:
 	 * 
 	 * Kleur moet tussen 1-6 liggen 
-	 * Zetsteen positie Y moet tussen 1-8 liggen 
-	 * Zetsteen positie X moet tussen 1-3 liggen
+	 * Zetsteen positie Y moet tussen 1-8 liggen
+	 * Zetsteen positie X moet tussen 1-3 liggen 
+	 * 			
 	 * 			~sverre lippens en Jelle Van Horen 2025~
 	 */
+
+	//kleur-----------------------------------------------------------
 	
+	@ParameterizedTest
+	@ValueSource(ints = { 022, -121, 031 })
+	void plaatsNeer_KleurTeLaag_werptExceptie(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+
+		assertThrows(IllegalArgumentException.class, () -> zetsteen.plaatsNeer(positie));
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 121, 322, 633 })
+	void plaatsNeer_KleurGrootGenoeg_KentPositieToe(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+		zetsteen.plaatsNeer(positie);
+
+		assertEquals(positie, zetsteen.getPositie());
+
+	}
 	
+	@ParameterizedTest
+	@ValueSource(ints = { 722, 821, 931 })
+	void plaatsNeer_KleurTeGroot_werptExceptie(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+
+		assertThrows(IllegalArgumentException.class, () -> zetsteen.plaatsNeer(positie));
+
+	}
+	
+	//vertical-----------------------------------------
+
+	@ParameterizedTest
+	@ValueSource(ints = { 502, -321 })
+	void plaatsNeer_VerticalePositieTeLaag_werptExceptie(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+
+		assertThrows(IllegalArgumentException.class, () -> zetsteen.plaatsNeer(positie));
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 111, 352, 683 })
+	void plaatsNeer_VerticalePositieGenoeg_KentPositieToe(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+		zetsteen.plaatsNeer(positie);
+
+		assertEquals(positie, zetsteen.getPositie());
+
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = { 592, 3101 })
+	void plaatsNeer_VerticalePositieTeGroot_werptExceptie(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+
+		assertThrows(IllegalArgumentException.class, () -> zetsteen.plaatsNeer(positie));
+
+	}
+	
+	//horizontal-------------------------------------------------------------
+
+	@ParameterizedTest
+	@ValueSource(ints = { 220, -121 })
+	void plaatsNeer_HorizontalePositieTeLaag_werptExceptie(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+
+		assertThrows(IllegalArgumentException.class, () -> zetsteen.plaatsNeer(positie));
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = { 121, 322, 633 })
+	void plaatsNeer_HorizontalePositieGrootGenoeg_KentPositieToe(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+		zetsteen.plaatsNeer(positie);
+		
+		assertEquals(positie, zetsteen.getPositie());
+
+	}
+	
+	@ParameterizedTest
+	@ValueSource(ints = { 224, 126 , 539 })
+	void plaatsNeer_HorizontalePositieTeGroot_werptExceptie(int positie) {
+
+		Zetsteen zetsteen = new Zetsteen();
+
+		assertThrows(IllegalArgumentException.class, () -> zetsteen.plaatsNeer(positie));
+
+	}
 
 }
