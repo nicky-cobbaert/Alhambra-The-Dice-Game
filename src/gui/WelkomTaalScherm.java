d2e17286dce218c604655cdf82a5767fde027571
Sverre Lippens <Sverre.Lippens@student.hogent.be>
2025-03-28 11:59:56 +0100

    Begin van beginscherm

--- src/gui/WelkomTaalScherm.java
+++ src/gui/WelkomTaalScherm.java
@@ -14,31 +14,6 @@ public class WelkomTaalScherm extends VBox{
 
 	public WelkomTaalScherm(DomeinController dc) {
 		this.dc = dc;
-		buildGui();
-	}
-	
-	private void buildGui() {
-		this.setPadding(new Insets(20));
-
-		setWidth(100);
-		setAlignment(Pos.CENTER);
-		
-		Label label = new Label("Welkom");
-		Label labelOnder = new Label("onder");
-		
-		Button knopNL = new Button("Speel");
-		Button knopENG = new Button("Play");
-		
-		label.setText("Welkom bij/Welcome by");
-		labelOnder.setText("Alhambra: The Dice Game!");
-		knopNL.setText("Speel het spel!");
-		knopENG.setText("Play the game!");
-		
-		VBox vbox = new VBox(20,label,labelOnder);
-		HBox hbox = new HBox(50,knopNL,knopENG);
-		
-		this.getChildren().add(vbox);
-		this.getChildren().add(hbox);
 	}
 	
 }
