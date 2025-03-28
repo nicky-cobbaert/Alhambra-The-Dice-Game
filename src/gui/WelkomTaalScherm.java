f057f5187c9068dad794ed6c9aa4b9d628ba5c99
Sverre Lippens <Sverre.Lippens@student.hogent.be>
2025-03-28 15:54:55 +0100

    Werkend beginscherm
    Nu hebben we toch al een deftig beginscherm!

--- src/gui/WelkomTaalScherm.java
+++ src/gui/WelkomTaalScherm.java
@@ -1,19 +1,49 @@
 package gui;
 
-import domein.DomeinController;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import java.io.IOException;
 
-public class WelkomTaalScherm extends VBox{
+import domein.DomeinController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
+public class WelkomTaalScherm extends GridPane{
+	
+	private void loadFxmlScreen(String name) {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(name));
+		loader.setRoot(this);
+		loader.setController(this);
+		try {
+			loader.load();
+		} catch (IOException ex) {
+			throw new RuntimeException(ex);
+		}
+	}
 	
 	private final DomeinController dc;
 
 	public WelkomTaalScherm(DomeinController dc) {
 		this.dc = dc;
+		loadFxmlScreen("WelkomTaalScherm.fxml");
 	}
+	
+
+    @FXML
+    private Button KnopENG;
+
+    @FXML
+    private Button KnopNL;
+
+    @FXML
+    void KnopENGklik(ActionEvent event) {
+
+    }
+
+    @FXML
+    void KnopNLklik(ActionEvent event) {
+
+    }
 	
 }
