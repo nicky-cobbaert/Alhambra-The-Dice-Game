fa0a4861c30384ff09c552847f429d588b71c6d4
Nicky Cobbaert <Nicky.Cobbaert@student.hogent.be>
2025-04-04 16:33:15 +0200

    gebruikerKiesScherm aangemaakt

---
+++ src/gui/GebruikerKiesScherm.java
@@ -0,0 +1,25 @@
+package gui;
+
+import java.io.IOException;
+
+import domein.DomeinController;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
+
+public class GebruikerKiesScherm extends BorderPane{
+
+	
+	
+	
+	
+	
+}
