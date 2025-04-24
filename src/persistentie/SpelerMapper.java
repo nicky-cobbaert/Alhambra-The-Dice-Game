1117ad78720be6161e94c3d884c8f971cd1d7da3
Jelle Van Horen <jelle.vanhoren@student.hogent.be>
2025-04-24 14:41:50 +0200

    Database werkt weer

--- src/persistentie/SpelerMapper.java
+++ src/persistentie/SpelerMapper.java
@@ -18,17 +18,17 @@ public class SpelerMapper {
     
     private static final String UPDATE_GEWONNEN = "UPDATE speler SET aantalGewonnen = aantalGewonnen + 1 WHERE gebruikers like ?";
     private static final String UPDATE_GESPEELD = "UPDATE speler SET aantalGespeeld = aantalGespeeld + 1 WHERE gebruikers like ?";
-    private List<Speler> offlineSpelers;
+//    private List<Speler> offlineSpelers;
     
     public SpelerMapper() {
-    	offlineSpelers = new ArrayList<Speler>();
-		offlineSpelers.add(new Speler("JelleVH",2004));
-		offlineSpelers.add(new Speler("NickyCob", 2006));
-		offlineSpelers.add(new Speler("SverreL", 2006));
-		offlineSpelers.add(new Speler("LarsDW", 2006));
-		offlineSpelers.add(new Speler("WoutGh", 2006));
-		offlineSpelers.add(new Speler("NickyCob2", 2006));
-		offlineSpelers.add(new Speler("NickyCobExtra", 2006));
+//    	offlineSpelers = new ArrayList<Speler>();
+//		offlineSpelers.add(new Speler("JelleVH",2004));
+//		offlineSpelers.add(new Speler("NickyCob", 2006));
+//		offlineSpelers.add(new Speler("SverreL", 2006));
+//		offlineSpelers.add(new Speler("LarsDW", 2006));
+//		offlineSpelers.add(new Speler("WoutGh", 2006));
+//		offlineSpelers.add(new Speler("NickyCob2", 2006));
+//		offlineSpelers.add(new Speler("NickyCobExtra", 2006));
 	}
             
     public void voegToe(Speler speler) 
@@ -52,6 +52,12 @@ public class SpelerMapper {
     public Speler geefSpeler(String gebruikersnaam) {
         Speler speler = null;
 
+//        for(Speler s: offlineSpelers) {
+//        	if(s.getGebruikersnaam() == gebruikersnaam) {
+//        		speler = s;
+//        		break;
+//        	}
+//        }
         try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL);
                 PreparedStatement query = conn.prepareStatement(GEEF_SPELER)) {
             query.setString(1, gebruikersnaam);
@@ -68,7 +74,7 @@ public class SpelerMapper {
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
-
+//
         return speler;
     }
 
@@ -77,26 +83,26 @@ public class SpelerMapper {
     
     public List<Speler> geefAlleSpelers(){
     	
-    	return offlineSpelers;
-//    	List<Speler> alleSpelers = new ArrayList<Speler>();
-//    	try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL);
-//    			PreparedStatement query = conn.prepareStatement("SELECT * FROM speler")){
-//    		try (ResultSet rs = query.executeQuery()) {
-//    			
-//    			while(rs.next())
-//    			{
-//    				String gebruikersnaam = rs.getString("Gebruikers");
-//    				int geboortejaar = rs.getInt("geboortejaar");
-//                    int aantalGewonnen = rs.getInt("aantalGewonnen");
-//                    int aantalGespeeld = rs.getInt("aantalGespeeld");
-//                    
-//                    alleSpelers.add(new Speler(gebruikersnaam,geboortejaar,aantalGewonnen,aantalGespeeld));
-//    			}
-//    		}
-//    	}catch (SQLException ex) {
-//    		throw new RuntimeException(ex);
-//    	}
-//    	return alleSpelers;
+    	//return offlineSpelers;
+    	List<Speler> alleSpelers = new ArrayList<Speler>();
+    	try (Connection conn = DriverManager.getConnection(Connectie.JDBC_URL);
+    			PreparedStatement query = conn.prepareStatement("SELECT * FROM speler")){
+    		try (ResultSet rs = query.executeQuery()) {
+    			
+    			while(rs.next())
+    			{
+    				String gebruikersnaam = rs.getString("Gebruikers");
+    				int geboortejaar = rs.getInt("geboortejaar");
+                    int aantalGewonnen = rs.getInt("aantalGewonnen");
+                    int aantalGespeeld = rs.getInt("aantalGespeeld");
+                    
+                    alleSpelers.add(new Speler(gebruikersnaam,geboortejaar,aantalGewonnen,aantalGespeeld));
+    			}
+    		}
+    	}catch (SQLException ex) {
+    		throw new RuntimeException(ex);
+    	}
+    	return alleSpelers;
     }
     
     public void updateGewonnen(String gebruikersnaam) {
