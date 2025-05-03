27de0e5d0cbb214b3b930208974b9dcaf140a200
Sverre Lippens <Sverre.Lippens@student.hogent.be>
2025-05-03 16:58:56 +0200

    Achtergrond kiesKleur en spelbord final

--- src/gui/KiesDobbelsteenKleurScherm.java
+++ src/gui/KiesDobbelsteenKleurScherm.java
@@ -7,6 +7,12 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import utils.DobbelsteenKleur;
@@ -44,6 +50,15 @@ public class KiesDobbelsteenKleurScherm extends GridPane {
 		zetKnopCorrect(groeneKleurKnop, DobbelsteenKleur.GROEN);
 		zetKnopCorrect(paarseKleurKnop, DobbelsteenKleur.PAARS);
 		zetKnopCorrect(rodeKleurKnop, DobbelsteenKleur.ROOD);
+		
+		Image foto = new Image(getClass().getResource("/images/KiesKleur.png").toExternalForm());
+
+		BackgroundSize size = new BackgroundSize(100, 100, true, true, true, false);
+
+		BackgroundImage achtergrond = new BackgroundImage(foto, BackgroundRepeat.NO_REPEAT,
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, size);
+
+		this.setBackground(new Background(achtergrond));
 	}
 
 	
