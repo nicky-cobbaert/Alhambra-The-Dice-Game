899a7b3682fb35cb58242842281cd1086f61fe48
nicky-cobbaert <nickycobbaert@gmail.com>
2026-06-24 21:31:28 +0200

    cleanup

--- src/persistentie/Connectie.java
+++ src/persistentie/Connectie.java
@@ -10,8 +10,10 @@ public class Connectie {
 
 	// public static final String JDBC_URL =
 	// "jdbc:mysql://vichogent.be:40094/G100?user=G100&password=rNwn1z4CpVBSBYRzkPZwz7SkBcf6efrW";
-	// --> orginele ik heb het aangepast naar ons ding
+	// --> orgineel
 
+	//	SERVER STAAT NIET MEER ONLINE, DE ONLINE VERSIE VAN HET SPEL WERKT DAN OOK NIET MEER!
+	//	-> ik weet dat deze gegevens hier normaal gezien niet thuishoren in verband met veiligheidsredenen.
 	public static String JDBC_URL = "jdbc:mysql://vichogent.be:40094/G74?user=G74&password=RyYNGVMqAI0m06amurTA0MQwfm3yG8XZ";
 
 	// als offlinemodus aanstaat gaat de jdbc module het lokale databank bestand in
