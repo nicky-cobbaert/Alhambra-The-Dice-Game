b9ccce3d4e05fccb9ac2c05c111aff3e9c40f22c
Nicky Cobbaert <NickyCobbaert@gmail.com>
2026-09-14 20:26:54 +0200

    changed pom

---
+++ src/main/Launcher.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // This calls the main method inside StartUpGui
+        StartUpGui.main(args);
+    }
+}
